0a560b8fdb8ff9a881a523e5b7382dc6328b08ee
Bryan Jensen <bryanawjensen@gmail.com>
2014-03-22 12:11:57 -0400

    Dicking around with layout options

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 $(function() {
+	$('a').click(function(event) {
+		event.preventDefault();
+
+		$('body, html').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 200);
+	});
+	// $('body').scrollTo('#second-screen');
 });
